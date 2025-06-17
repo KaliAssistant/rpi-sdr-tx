80f297d40199fe78a05fcc160d82c90d5c08e71c
kaliassistant <1325077@gmail.com>
2025-06-17 13:54:23 +0800

    update build script

--- rpi02w.sh
+++ rpi02w.sh
@@ -2,21 +2,7 @@
 
 set -eE
 
-RPI_SDR_TX_VERSION="v1.0.0"
-REPO_PWD=`pwd`
-work_dir="${REPO_PWD}/base/working"
-
-# Use 2025-05-13 raspbian armhf lite
-RASPBIAN_DOWNLOAD_URL="https://downloads.raspberrypi.com/raspios_lite_armhf/images/raspios_lite_armhf-2025-05-13/2025-05-13-raspios-bookworm-armhf-lite.img.xz"
-
-# GPG signature
-RASPBIAN_DOWNLOAD_SIG="${REPO_PWD}/img_sig/2025-05-13-raspios-bookworm-armhf-lite.img.xz.sig"
-
-# raspberrypi official downloads gpg public key
-RASPBIAN_DOWNLOAD_GPG_PBKEY="${REPO_PWD}/img_sig/54c3dd610d9d1b4af82a37758738cd6b956f460c.asc"
-
-RASPBIAN_DOWNLOADED_IMAGE="${REPO_PWD}/downloads/raspios-bookworm-armhf-lite.img.xz"
-
+source "$(pwd)/variables.sh"
 
 if [ "$EUID" -ne 0 ]
   then echo -e "\e[1;31m[ABORT] Please run as root !\e[0m"
@@ -34,7 +20,7 @@ do_trap_cleanup() {
             umount -lf "$work_dir/$mp" 2>/dev/null || true
         fi
     done
-    loop_device="$(losetup -j "${REPO_PWD}/base/base_image.img" | cut -d':' -f1)"
+    loop_device="$(losetup -j "$BASE_IMAGE" | cut -d':' -f1)"
     if [ -n "$loop_device" ]; then
         echo "Detaching loop device: $loop_device"
         losetup -d "$loop_device" 2>/dev/null || true
@@ -54,7 +40,7 @@ do_fail_cleanup() {
             umount -lf "$work_dir/$mp" 2>/dev/null || true
         fi
     done
-    loop_device="$(losetup -j "${REPO_PWD}/base/base_image.img" | cut -d':' -f1)"
+    loop_device="$(losetup -j "$BASE_IMAGE" | cut -d':' -f1)"
     if [ -n "$loop_device" ]; then
         echo "Detaching loop device: $loop_device"
         losetup -d "$loop_device" 2>/dev/null || true
@@ -91,9 +77,9 @@ echo -e "\n\e[0;32m[INFO]\e[1;37m Start Build script, need internet connection.\
 
 do_init_cleanup() {
     echo -e "\e[0;32m[INFO]\e[1;37m Init cleanup...\e[0m"
-    trash_chksum_files=$(find "${REPO_PWD}/base" -name "*.sha256sum")
-    trash_xz_files=$(find "${REPO_PWD}/base" -name "*.xz")
-    trash_img_files=$(find "${REPO_PWD}/base" -name "*.img")
+    trash_chksum_files=$(find "$base_dir" -name "*.sha256sum")
+    trash_xz_files=$(find "$base_dir" -name "*.xz")
+    trash_img_files=$(find "$base_dir" -name "*.img")
 
     [ ! -n "$trash_chksum_files" ] || rm -f $trash_chksum_files
     [ ! -n "$trash_xz_files" ] || rm -f $trash_xz_files
@@ -107,37 +93,49 @@ do_apt_update() {
     apt install -y git curl qemu-user qemu-user-static uuid-runtime pixz xz-utils xxd
 }
 
-do_read_builder_txt() {
-    echo -e "\e[0;32m[INFO]\e[1;37m Read builder.txt and variables...\e[0m"
-    source "${REPO_PWD}/variables.sh"
-}
-
 do_download_raspbian() {
     echo -e "\e[0;32m[INFO]\e[1;37m Download raspbian image\e[0m"
-    wget -O - "$RASPBIAN_DOWNLOAD_URL" > "${REPO_PWD}/downloads/raspios-bookworm-armhf-lite.img.xz"
+    wget -O - "$RASPBIAN_DOWNLOAD_URL" > "$RASPBIAN_DOWNLOADED_IMAGE"
     echo -e "\e[0;32m[INFO\e[1;37m Verify Download...\e[0m"
     gpg --import "$RASPBIAN_DOWNLOAD_GPG_PBKEY"
-    gpg --verify "$RASPBIAN_DOWNLOAD_SIG" "${REPO_PWD}/downloads/raspios-bookworm-armhf-lite.img.xz"
+    gpg --verify "$RASPBIAN_DOWNLOAD_SIG" "$RASPBIAN_DOWNLOADED_IMAGE"
     echo -e "\e[0;32m[INFO]\e[1;37m Decompress image file to base...\e[0m"
     xz -dk "$RASPBIAN_DOWNLOADED_IMAGE"
-    mv "${REPO_PWD}/downloads/raspios-bookworm-armhf-lite.img" "${REPO_PWD}/base/base_image.img"
+    cp "$RASPBIAN_DECOMPRESSED_IMAGE" "$BASE_IMAGE"
 }
 
 do_dect_download_exists() {
     echo -e "\e[0;32m[INFO]\e[1;37m Check downloads folder...\e[0m"
-    if [ -f "$RASPBIAN_DOWNLOADED_IMAGE" ]; then
+
+    if [ -f "$RASPBIAN_DECOMPRESSED_IMAGE" ]; then
+        echo -e "\e[0;32m[INFO]\e[1;37m Found Decompressed image!\e[0m"
+        echo -e "\e[0;32m[INFO]\e[1;37m Verify Image...\e[0m"
+        verify_shasum=$(sha256sum "$RASPBIAN_DECOMPRESSED_IMAGE" | awk -F' ' '{print $1}')
+        [ "$verify_shasum" == "$RASPBIAN_DECOMPRESSED_IMAGE_SHA256SUM" ] || EXIST_DIMAGE_ERR_CHKSUM="ERROR"
+        if [ -n "$EXIST_DIMAGE_ERR_CHKSUM" ]; then
+            echo -e "\e[1;33m[WARN]\e[0;33m SHA256SUM NOT MATCH ! REDOWNLOAD THE IMAGE...\e[0m"
+            do_download_raspbian
+            return 0
+        fi
+        
+        echo -e "\e[0;32m[INFO]\e[1;37m Copy image file to base...\e[0m"
+        cp "$RASPBIAN_DECOMPRESSED_IMAGE" "$BASE_IMAGE"
+        return 0
+        
+
+    elif [ -f "$RASPBIAN_DOWNLOADED_IMAGE" ]; then
         echo -e "\e[0;32m[INFO]\e[1;37m Found Downloaded image!\e[0m"
         echo -e "\e[0;32m[INFO]\e[1;37m Verify Image...\e[0m"
         gpg --import "$RASPBIAN_DOWNLOAD_GPG_PBKEY"
         gpg --verify "$RASPBIAN_DOWNLOAD_SIG" "$RASPBIAN_DOWNLOADED_IMAGE" || EXIST_IMAGE_GPG_ERROR="ERROR"
         if [ -n "$EXIST_IMAGE_GPG_ERROR" ]; then
-              echo -e "\e[1;33m[WARN]\e[0;33m GPG VERIFY RETURN NON-ZERO EXIT-CODE ! REDOWNLOAD THE IMAGE...\e[0m"
-              do_download_raspbian
-          return 0
+            echo -e "\e[1;33m[WARN]\e[0;33m GPG VERIFY RETURN NON-ZERO EXIT-CODE ! REDOWNLOAD THE IMAGE...\e[0m"
+            do_download_raspbian
+            return 0
         fi
         echo -e "\e[0;32m[INFO]\e[1;37m Decompress image file to base...\e[0m"
         xz -dk "$RASPBIAN_DOWNLOADED_IMAGE"
-        mv "${REPO_PWD}/downloads/raspios-bookworm-armhf-lite.img" "${REPO_PWD}/base/base_image.img"
+        cp "$RASPBIAN_DECOMPRESSED_IMAGE" "$BASE_IMAGE"
         return 0
     else
         echo -e "\e[0;32m[INFO]\e[1;37m Image not found...\e[0m"
@@ -149,14 +147,14 @@ do_dect_download_exists() {
 
 do_resize_img_file() {
     echo -e "\e[0;32m[INFO]\e[1;37m Resize img file to 10G\e[0m"
-    truncate -s 10G "${REPO_PWD}/base/base_image.img"
+    truncate -s 10G "$BASE_IMAGE"
 }
 
 do_chroot_to_working() {
     echo -e "\e[0;32m[INFO]\e[1;37m Chroot to work_dir\e[0m"
 
     echo -e "\e[0;32m[INFO]\e[1;37m Mount img to loop...\e[0m"
-    loop_device=$(losetup -Pf --show "${REPO_PWD}/base/base_image.img")
+    loop_device=$(losetup -Pf --show "$BASE_IMAGE")
     
     # kpartx -v -a "${REPO_PWD}/base/base_image.img" || do_failexit
     #loop_device=`losetup --list | grep "${REPO_PWD}/base/base_image.img" | cut -d ' ' -f1 | cut -d'/' -f3`
@@ -170,7 +168,7 @@ do_chroot_to_working() {
 
     losetup -d "$loop_device"
 
-    loop_device=$(losetup -Pf --show "${REPO_PWD}/base/base_image.img")
+    loop_device=$(losetup -Pf --show "$BASE_IMAGE")
 
     # loop_device=`losetup --list | grep "${REPO_PWD}/base/base_image.img" | cut -d ' ' -f1 | cut -d'/' -f3`
     mapper_loop_device="${loop_device}p"
@@ -313,7 +311,7 @@ EOF
 do_minsize_imgfile() {
     echo -e "\e[0;32m[INFO]\e[1;37m Resize build imgage to MIN_SIZE\e[0m"
 
-    loop_device=$(losetup -Pf --show "${REPO_PWD}/base/base_image.img")
+    loop_device=$(losetup -Pf --show "$BASE_IMAGE")
     
     ROOT_PART="${loop_device}p2"
 
@@ -327,14 +325,14 @@ do_minsize_imgfile() {
     resize2fs "$ROOT_PART" "$NEW_SIZE"
 
     # Get original start sector of rootfs partition
-    START_SECTOR=$(fdisk -l "${REPO_PWD}/base/base_image.img" | awk '/Linux/ {print $2}')
-    SECTOR_SIZE=$(fdisk -l "${REPO_PWD}/base/base_image.img" | awk '/Units:/ {print $8}')
+    START_SECTOR=$(fdisk -l "$BASE_IMAGE" | awk '/Linux/ {print $2}')
+    SECTOR_SIZE=$(fdisk -l "$BASE_IMAGE" | awk '/Units:/ {print $8}')
 
     # Detach loop to use parted
     losetup -d "$loop_device"
 
     # Resize partition table
-    parted "${REPO_PWD}/base/base_image.img" ---pretend-input-tty <<EOF
+    parted "$BASE_IMAGE" ---pretend-input-tty <<EOF
 unit s
 resizepart 2 $(( START_SECTOR + NEW_SIZE * 8 - 1 ))
 Yes
@@ -342,59 +340,50 @@ quit
 EOF
 
     # Reattach and do a final resize to fill partition
-    loop_device=$(losetup -Pf --show "${REPO_PWD}/base/base_image.img")
+    loop_device=$(losetup -Pf --show "$BASE_IMAGE")
     ROOT_PART="${loop_device}p2"
     resize2fs "$ROOT_PART"
     losetup -d "$loop_device"
 
     # Truncate image to end of partition
     END_OFFSET=$(( (START_SECTOR + NEW_SIZE * 8) * SECTOR_SIZE ))
-    truncate -s "$END_OFFSET" "${REPO_PWD}/base/base_image.img"
-
-    echo -e "\e[0;32m[INFO]\e[1;37m Generate sha256sum: ${REPO_PWD}/base/base_image.img"
-    shasum -a 256 "${REPO_PWD}/base/base_image.img" >"${REPO_PWD}/base/base_image.img.sha256sum"
+    truncate -s "$END_OFFSET" "$BASE_IMAGE"
 }
 
 do_xz_compress() {
+    echo -e "\e[0;32m[INFO]\e[1;37m Generate base image sha256sum...\e[0m"
+    raw_img_sha256sum=$(sha256sum "$BASE_IMAGE" | awk -F' ' '{print $1}')
     echo -e "\e[0;32m[INFO]\e[1;37m Running XZ Compress...\e[0m"
-    pixz -p `nproc` "${REPO_PWD}/base/base_image.img"
-    img="${REPO_PWD}/base/base_image.img.xz"
+    pixz -p `nproc` "$BASE_IMAGE"
+    img="${BASE_IMAGE}.xz"
 
     chmod 0644 "$img"
 
-    echo -e "\e[0;32m[INFO]\e[1;37m Generate sha256sum: ${REPO_PWD}/base/base_image.img.xz"
+    echo -e "\e[0;32m[INFO]\e[1;37m Generate sha256sum: $img"
+    xz_img_sha256sum=$(sha256sum "$img" | awk -F' ' '{print $1}')
 
-    shasum -a 256 "$img" >"${REPO_PWD}/base/base_image.img.xz.sha256sum"
-
-    mkdir -p "${REPO_PWD}/build-image"
+    mkdir -p "${repo_dir}/build-image"
     BUILD_TIME="$(date +%F_%H-%M-%S)"
-    img_name="${BUILD_TIME}_rpi-sdr-tx_pi02w_armhf.img.xz"
-    mv "$img" "${REPO_PWD}/build-image/${img_name}"
-    cp "${REPO_PWD}/base/base_image.img.sha256sum" "${REPO_PWD}/build-image/${BUILD_TIME}_rpi-sdr-tx_pi02w_armhf.img.sha256sum"
-    cp "${REPO_PWD}/base/base_image.img.xz.sha256sum" "${REPO_PWD}/build-image/${BUILD_TIME}_rpi-sdr-tx_pi02w_armhf.img.xz.sha256sum"
 
-    echo -e "\e[0;32m[INFO]\e[1;37m All done! Your image is ${REPO_PWD}/build-image/${img_name}  Have a nice day!\e[0m"
+    raw_img_name="${BUILD_TIME}_rpi-sdr-tx_pi02w_armhf.img"
+    img_name="${raw_img_name}.xz"
+    cp "$img" "${repo_dir}/build-image/${img_name}"
+
+    echo "${raw_img_sha256sum}  ${raw_img_name}" > "${repo_dir}/build-image/${raw_img_name}.sha256sum"
+    echo "${xz_img_sha256sum}  ${img_name}" > "${repo_dir}/build-image/${img_name}.sha256sum"
+
+    echo -e "\e[0;32m[INFO]\e[1;37m All done! Your image is ${repo_dir}/build-image/${img_name} Have a nice day!\e[0m"
 }
 
-do_done_cleanup() {
-    trash_chksum_files=$(find "${REPO_PWD}/base" -name "*.sha256sum")
-    trash_xz_files=$(find "${REPO_PWD}/base" -name "*.xz")
-    trash_img_files=$(find "${REPO_PWD}/base" -name "*.img")
-
-    [ ! -n "$trash_chksum_files" ] || rm -f $trash_chksum_files
-    [ ! -n "$trash_xz_files" ] || rm -f $trash_xz_files
-    [ ! -n "$trash_img_files" ] || rm -f $trash_img_files
-}
 
 do_init
 do_init_cleanup
 do_apt_update
-do_read_builder_txt
 do_dect_download_exists
 do_resize_img_file
 do_chroot_to_working
 do_minsize_imgfile
 do_xz_compress
-do_done_cleanup
+
 exit 0
 
--- variables.sh
+++ variables.sh
@@ -1,6 +1,31 @@
 #!/usr/bin/env bash
 
+RPI_SDR_TX_VERSION="v1.0.0"
+
 repo_dir="$(pwd)"
+
+base_dir="${repo_dir}/base"
+
+work_dir="${base_dir}/working"
+
+# Use 2025-05-13 raspbian armhf lite
+RASPBIAN_DOWNLOAD_URL="https://downloads.raspberrypi.com/raspios_lite_armhf/images/raspios_lite_armhf-2025-05-13/2025-05-13-raspios-bookworm-armhf-lite.img.xz"
+
+# GPG signature
+RASPBIAN_DOWNLOAD_SIG="${repo_dir}/img_sig/2025-05-13-raspios-bookworm-armhf-lite.img.xz.sig"
+
+# raspberrypi official downloads gpg public key
+RASPBIAN_DOWNLOAD_GPG_PBKEY="${repo_dir}/img_sig/54c3dd610d9d1b4af82a37758738cd6b956f460c.asc"
+
+RASPBIAN_DOWNLOADED_IMAGE="${repo_dir}/downloads/raspios-bookworm-armhf-lite.img.xz"
+
+RASPBIAN_DECOMPRESSED_IMAGE="${repo_dir}/downloads/raspios-bookworm-armhf-lite.img"
+
+RASPBIAN_DECOMPRESSED_IMAGE_SHA256SUM="27afb17f14c525e8aa56ad78ae306d581f62bf2531a24752ce97e1696883dd64"
+
+BASE_IMAGE="${base_dir}/base_image.img"
+
+
 
 usb_serial=${usb_serial:-$(head -c 16 /dev/urandom | xxd -p -u | tr -d '\n')}
 
